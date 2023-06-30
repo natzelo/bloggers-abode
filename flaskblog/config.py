3d29115701e92ce3b6de16218b5bd3ade0caf194
Akshat Agrawal <akshat@Akshats-MacBook-Air.local>
2023-06-30 18:53:21 +0530

    Blogger's Abode v1.0

---
+++ flaskblog/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('TEST_EMAIL')
+    MAIL_PASSWORD = os.environ.get('TEST_EMAIL_PASS')
